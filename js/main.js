2d9180183e915c8bb6bf00179147a1bc3d213bd9
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-06-07 19:21:41 +0300

    Add todoList methods

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 const todoList = {
-  todos: dummyData.todos
+  todos: dummyData.todos,
+
+  addItem(name, completed) {
+    const item = { id: Date.now(), name, completed }
+
+    this.todos.push(item)
+    
+    console.table(this.todos)
+  },
+
+  removeItem(id) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].id === id) {
+        this.todos.splice(i, 1)
+        break;
+      }
+    }
+    
+    console.table(this.todos)
+  }
 }
 
 console.table(todoList.todos)
