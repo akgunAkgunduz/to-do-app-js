571da6d14f58b825de0226ddf0bbedfc155486d6
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-06-08 20:39:32 +0300

    Update dummy data

--- js/dummyData.js
+++ js/dummyData.js
@@ -1,8 +1,9 @@
 const dummyData = {
   todos: [
-    { id: 1558439676665, name: 'Buy coffee beans', completed: true },
-    { id: 1558439676667, name: 'Practice piano', completed: false },
-    { id: 1558439676666, name: 'Go to library', completed: true },
-    { id: 1558439676668, name: 'Do homework', completed: false },
+    { id: 1558439676665, name: 'Practice piano', completed: true },
+    { id: 1558439676666, name: 'Study modes', completed: false },
+    { id: 1558439676667, name: 'Buy coffee beans', completed: false },
+    { id: 1558439676668, name: 'Go to library', completed: false },
+    { id: 1558439676669, name: 'Do homework', completed: true },
   ]	
 }
