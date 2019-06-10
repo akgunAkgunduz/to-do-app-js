3dcd9cdb7cf9fc003ca2064a7a1a199e96afc087
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-06-10 06:44:17 +0300

    Add addItem and removeItem methods to view object

--- js/main.js
+++ js/main.js
@@ -23,8 +23,34 @@ const todoList = {
 
 const view = {
   grid: new Muuri('#grid', { dragEnabled: true }),
+
   list: document.getElementById('#grid'),
-  input: document.getElementById('#add-item')
+  
+  input: document.getElementById('#add-item'),
+
+  addItem({ id, name, completed }) {
+    const itemDiv = document.createElement('div')
+    itemDiv.classList.add('item')
+    itemDiv.dataset.id = id
+
+    const itemContentDiv = document.createElement('div')
+    itemContentDiv.classList.add('item-content')
+
+    let todoContentDiv = document.createElement('div')
+    todoContentDiv.classList.add('todo-content')
+    todoContentDiv.innerText = name
+
+    itemContentDiv.appendChild(todoContentDiv)
+    itemDiv.appendChild(itemContentDiv)
+
+    this.grid.add(itemDiv)
+  },
+
+  removeItem(id) {
+    const item = document.querySelector(`.item[data-id="${id}"]`)
+
+    this.grid.remove(item, { removeElements: true })
+  }
 }
 
 console.table(todoList.todos)
