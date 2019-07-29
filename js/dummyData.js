03e924ee1601c37e967f0d8abee41d3d48191720
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-07-29 22:41:47 +0300

    Remove dummy data

--- js/dummyData.js
+++
@@ -1,9 +0,0 @@
-const dummyData = {
-  todos: [
-    { id: 1558439676665, name: 'Practice piano', completed: true },
-    { id: 1558439676666, name: 'Study modes', completed: false },
-    { id: 1558439676667, name: 'Buy coffee beans', completed: false },
-    { id: 1558439676668, name: 'Go to library', completed: false },
-    { id: 1558439676669, name: 'Do homework', completed: true },
-  ]	
-}
