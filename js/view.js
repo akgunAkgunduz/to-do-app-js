a4c9b89d21b84fe41c8f1e9bac4520ef51e0c2dd
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-07-31 14:51:06 +0300

    Refactor event handlers and listeners

--- js/view.js
+++ js/view.js
@@ -21,20 +21,15 @@ const view = {
   addItemToGrid({ id, name, completed }) {
     const itemDiv = document.createElement('div')
     const itemContentDiv = document.createElement('div')
-
     const handleContainer = document.createElement('div')
     const handle = document.createElement('div')
     const handleIcon = document.createElement('i')
-
     const toggle = document.createElement('div')
     const toggleLabel = document.createElement('label')
     const toggleCheckbox = document.createElement('input')
     const toggleDiv = document.createElement('div')
-
     const todoContentDiv = document.createElement('div')
-
     const editInput = document.createElement('input')
-
     const deleteButtonContainer = document.createElement('div')
     const deleteButton = document.createElement('button')
     const deleteButtonIcon = document.createElement('i')
@@ -46,30 +41,27 @@ const view = {
     itemContentDiv.dataset.id = id
 
     handle.className = 'handle hidden'
-
     handleIcon.className = 'fas fa-grip-vertical'
 
     toggle.className = 'toggle'
     toggle.dataset.id = id
-
     toggleCheckbox.type = 'checkbox'
     toggleCheckbox.dataset.id = id
     toggleCheckbox.checked = completed ? true : false
-
     toggleDiv.className = 'check-mark'
     toggleDiv.dataset.id = id    
 
     todoContentDiv.className = 'todo-content'
     todoContentDiv.innerText = name
     todoContentDiv.dataset.id = id
-
-    editInput.type = 'text'
-    editInput.className = 'edit hidden'
-    editInput.dataset.id = id
-
+    
     deleteButton.className = 'del-btn hidden'
     deleteButtonIcon.className = 'fas fa-trash'
     deleteButton.dataset.id = id
+    
+    editInput.type = 'text'
+    editInput.className = 'edit hidden'
+    editInput.dataset.id = id
 
     handle.appendChild(handleIcon)
     handleContainer.appendChild(handle)
@@ -88,24 +80,16 @@ const view = {
     itemContentDiv.appendChild(deleteButtonContainer)
     
     itemDiv.appendChild(itemContentDiv)
-    
     this.updateItemStyling(itemDiv, completed)
 
-    toggleDiv.addEventListener('click', this.handleToggleClick)
-    this.addEventListenersForEditInput(editInput)
-    deleteButton.addEventListener('click', this.handleDeleteButtonClick)
-
+    this.addEventListenersForTodoItem(itemDiv)    
     this.grid.add(itemDiv)
   },
 
   removeItemFromGrid(id) {
     const item = document.querySelector(`.item[data-id="${id}"]`)
-    const deleteButton = item.querySelector('.del-btn')
-    const toggleDiv = item.querySelector('.check-mark')
 
-    deleteButton.removeEventListener('click', this.handleDeleteButtonClick)
-    toggleDiv.removeEventListener('click', this.handleToggleClick)
-
+    this.removeEventListenersFromTodoItem(item)
     this.grid.remove(item, { removeElements: true })
   },
 
@@ -125,11 +109,44 @@ const view = {
     items.forEach((item, i) => item.getElement().dataset.index = i)
   },
 
+  // Event Handlers
+
+  handleInputKeydown(e) {
+    if (e.key === 'Enter') {
+      const name = e.target.value.trim()
+      
+      if (name !== '') {
+        controller.addTodo(name, false)
+        e.target.value = ''
+      }
+    }
+  },
+
+  handleGridDragReleaseEnd(item) {
+    const indices = []
+
+    const grid = item.getGrid()
+    const items = grid.getItems()
+
+    items.forEach(item => indices.push(item.getElement().dataset.index))
+
+    controller.reorderTodos(indices)
+  },
+
   handleToggleClick(e) {
     const listItem = e.target.closest('.item')
     const id = parseInt(listItem.dataset.id)
 
     controller.toggleTodoStatus(id, listItem)
+  },
+
+  handleTodoContentDoubleClick(e) {
+    const listItem = e.target.closest('.item')
+    const editInput = listItem.querySelector('.edit')
+
+    editInput.classList.remove('hidden')
+    editInput.value = e.target.innerText
+    editInput.focus()
   },
 
   handleDeleteButtonClick(e) {
@@ -138,61 +155,57 @@ const view = {
     controller.removeTodo(id)
   },
 
-  setUpEventListeners() {
-    this.input.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        const name = e.target.value.trim()
-        
-        if (name !== '') {
-          controller.addTodo(name, false)
-          e.target.value = ''
-        }
-      }
-    })
-
-    this.grid.on('dragReleaseEnd', (item) => {
-      const indices = []
-
-      const grid = item.getGrid()
-      const items = grid.getItems()
-
-      items.forEach(item => indices.push(item.getElement().dataset.index))
-
-      controller.reorderTodos(indices)
-    })
-
-    this.list.addEventListener('dblclick', (e) => {
-      if (e.target.matches('.todo-content')) {
-        const id = parseInt(e.target.dataset.id)
-        const editInput = document.querySelector(`.edit[data-id="${id}"]`)
-
-        editInput.classList.remove('hidden')
-        editInput.value = e.target.innerText
-        editInput.focus()
-      }
-    })
+  handleEditInputBlur(e) {
+    e.target.classList.add('hidden')
   },
 
-  addEventListenersForEditInput(input) {    
-    input.addEventListener('blur', (e) => {
-      e.target.classList.add('hidden')
-    })
+  handleEditInputKeyup(e) {
+    if (e.key === 'Enter') {
+      const id = parseInt(e.target.dataset.id)
+      const newName = e.target.value.trim()
 
-    input.addEventListener('keyup', (e) => {
-      if (e.key === 'Enter') {
-        const id = parseInt(e.target.dataset.id)
-        const newName = e.target.value.trim()
-
-        if (newName !== '') {
-          controller.updateTodoName(id, newName)
-          view.updateItemName(id, newName)
-          e.target.blur()
-        }
-      }
-
-      if (e.key === 'Escape') {
+      if (newName !== '') {
+        controller.updateTodoName(id, newName)
+        view.updateItemName(id, newName)
         e.target.blur()
       }
-    })    
+    }
+
+    if (e.key === 'Escape') {
+      e.target.blur()
+    }
+  },
+
+  // Event Listeners
+
+  setUpEventListeners() {
+    this.input.addEventListener('keydown', this.handleInputKeydown)
+    this.grid.on('dragReleaseEnd', this.handleGridDragReleaseEnd)
+  },
+
+  addEventListenersForTodoItem(item) {
+    const toggleDiv = item.querySelector('.check-mark')
+    const todoContentDiv = item.querySelector('.todo-content')
+    const deleteButton = item.querySelector('.del-btn')
+    const editInput = item.querySelector('.edit')
+
+    toggleDiv.addEventListener('click', this.handleToggleClick)
+    todoContentDiv.addEventListener('dblclick', this.handleTodoContentDoubleClick)
+    deleteButton.addEventListener('click', this.handleDeleteButtonClick) 
+    editInput.addEventListener('blur', this.handleEditInputBlur)
+    editInput.addEventListener('keyup', this.handleEditInputKeyup)
+  },
+
+  removeEventListenersFromTodoItem(item) {
+    const toggleDiv = item.querySelector('.check-mark')
+    const todoContentDiv = item.querySelector('.todo-content')
+    const deleteButton = item.querySelector('.del-btn')
+    const editInput = item.querySelector('.edit')
+
+    toggleDiv.removeEventListener('click', this.handleToggleClick)
+    todoContentDiv.removeEventListener('dblclick', this.handleTodoContentDoubleClick)
+    deleteButton.removeEventListener('click', this.handleDeleteButtonClick)
+    editInput.removeEventListener('blur', this.handleEditInputBlur)
+    editInput.removeEventListener('keyup', this.handleEditInputKeyup)
   }
 }
