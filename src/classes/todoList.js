78fae183c08e45380b493a26f5cb39dbe4b5064f
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-09-03 19:11:29 +0300

    Add TodoList class

---
+++ src/classes/todoList.js
@@ -0,0 +1,65 @@
+export default class TodoList {
+  constructor(todos) {
+    this.todos = todos
+  }
+
+  setItems(items) {
+    this.todos = [ ...items ]
+  }
+
+  addItem(name, completed) {
+    const item = { id: Date.now(), name, completed }
+
+    this.todos.push(item)
+    
+    console.table(this.todos)
+
+    return item
+  }
+
+  removeItem(id) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].id === id) {
+        this.todos.splice(i, 1)
+        break
+      }
+    }
+    
+    console.table(this.todos)
+  }
+
+  toggleItemStatus(id) {
+    let isCompleted = null
+
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].id === id) {
+        this.todos[i].completed = !this.todos[i].completed
+        isCompleted = this.todos[i].completed
+        break
+      }
+    }
+
+    console.table(this.todos)
+
+    return isCompleted
+  }
+
+  updateItemName(id, newName) {
+    for (let i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].id === id) {
+        this.todos[i].name = newName
+        break
+      }
+    }
+    
+    console.table(this.todos)
+  }
+
+  reorderItems(newIndices) {
+    const reorderedTodos = newIndices.map(index => this.todos[index])
+
+    this.setItems(reorderedTodos)
+
+    console.table(this.todos)
+  }
+}
